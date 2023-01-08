f47d950a1db089b74bd2f28a2d36470e87d5a5a1
tracer-ics2021 <tracer@njuics.org>
2023-01-08 21:17:38 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: e02d2c855e8f4966bb11c1cfc1f8d69d   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:17:38 up  8:35,  1 user,  load average: 3.17, 3.53, 3.00

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  //***********************************pa2********************************
+  // kbd->keydown = 0;
+  // kbd->keycode = AM_KEY_NONE;
+  uint32_t keyboard_code = inl(KBD_ADDR);
+  kbd->keydown = (keyboard_code & KEYDOWN_MASK ? true : false);
+  kbd->keycode = (~KEYDOWN_MASK) & keyboard_code;
+  //***********************************pa2********************************
 }
