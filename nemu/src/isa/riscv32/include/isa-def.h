1d29d3d453b9ed20cd8cf7893d67079e0e0eb575
tracer-ics2021 <tracer@njuics.org>
2023-01-09 21:54:11 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 540996759efb47de9b42576bf7974f62   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:54:11 up  9:36,  1 user,  load average: 0.30, 0.27, 0.31

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -59,13 +59,13 @@ typedef struct {
     struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
-      uint32_t imm11 : 1;
-      uint32_t imm4_1 : 4;
-      uint32_t funct3 : 3;
-      uint32_t rs1 : 5;
-      uint32_t rs2 : 5;
-      uint32_t imm10_5 : 6;
-      uint32_t imm12 : 1;
+      uint32_t imm11     : 1;
+      uint32_t imm4_1    : 4;
+      uint32_t funct3    : 3;
+      uint32_t rs1       : 5;
+      uint32_t rs2       : 5;
+      uint32_t  imm10_5  : 6;
+      uint32_t  imm12    : 1;
     } b;
     //************************************pa2**************************************
     uint32_t val;
--- nemu/src/isa/riscv32/include/isa-exec.h
+++ nemu/src/isa/riscv32/include/isa-exec.h
@@ -1,5 +1,3 @@
 #include "../instr/compute.h"
 #include "../instr/ldst.h"
 #include "../instr/special.h"
-#include "../instr/jump.h"
-#include "../instr/set.h"
