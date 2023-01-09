3d928c5637fc1ba536f857a4e4a77a8188c96bdc
tracer-ics2021 <tracer@njuics.org>
2023-01-09 13:55:13 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 540996759efb47de9b42576bf7974f62   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  13:55:14 up  3:36,  1 user,  load average: 1.25, 0.67, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,8 +2,16 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
+//*****************************pa2********************************
+#define SYNC_HEIGHT (VGACTL_ADDR)
+//*****************************pa2********************************
 void __am_gpu_init() {
+  int i;
+  int w = 0;  
+  int h = 0;  
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -8,7 +8,9 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   // kbd->keydown = 0;
   // kbd->keycode = AM_KEY_NONE;
   uint32_t keyboard_code = inl(KBD_ADDR);
+  // 最高位是判断是按下还是弹回
   kbd->keydown = (keyboard_code & KEYDOWN_MASK ? true : false);
+  // 其余位则表示按下的键
   kbd->keycode = (~KEYDOWN_MASK) & keyboard_code;
   //***********************************pa2********************************
 }
