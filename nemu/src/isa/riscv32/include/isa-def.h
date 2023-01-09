884249e9e564dde77dfe179e24718e53857c57f3
tracer-ics2021 <tracer@njuics.org>
2023-01-09 21:47:52 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 540996759efb47de9b42576bf7974f62   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:47:52 up  9:30,  1 user,  load average: 0.51, 0.40, 0.35

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -47,15 +47,14 @@ typedef struct {
       uint32_t imm10_1   :10;
       uint32_t imm20     : 1;
     } j;
-    struct
-    {
+    struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
-      uint32_t rd : 5;
-      uint32_t funct3 : 3;
-      uint32_t rs1 : 5;
-      uint32_t rs2 : 5;
-      int32_t simm11_5 : 7;
+      uint32_t rd        : 5;
+      uint32_t funct3    : 3;
+      uint32_t rs1       : 5;
+      uint32_t rs2       : 5;
+      int32_t  funct7    : 7;
     } r;
     struct
     {
