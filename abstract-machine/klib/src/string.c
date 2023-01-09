628225e02bcebfeb9420804b6dbd816f78e53be1
tracer-ics2021 <tracer@njuics.org>
2023-01-09 14:17:10 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 540996759efb47de9b42576bf7974f62   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  14:17:10 up  3:57,  1 user,  load average: 0.32, 0.30, 0.27

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,134 +1,116 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdint.h>
-
+#include <stdio.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  //****************************************pa2*******************************************
-  size_t i = 0;
-  for (; s[i] != 0; i++);
-  return i;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
+  size_t cnt = 0, i = 0;
+  while(s[i++] != '\0'){
+    cnt ++;
+  }
+  
+  return cnt;
 }
 
 char *strcpy(char *dst, const char *src) {
-  //****************************************pa2*******************************************
   size_t i = 0;
-  for (; src[i] != 0; i++) {
+  for(; src[i] != '\0';++i){
     dst[i] = src[i];
   }
-  dst[i] = 0;
+  dst[i] = '\0';
   return dst;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  //****************************************pa2*******************************************
   size_t i = 0;
-  for (; i < n && src[i] != 0; i++) {
+  for(; (i < n) && (src[i]!='\0') ; ++i){
     dst[i] = src[i];
-  }
-  for (; i < n; i++) {
-    dst[i] = 0;
+  }  for(;i < n; i++){
+    dst[i] = '\0';
   }
   return dst;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
 }
 
 char *strcat(char *dst, const char *src) {
-  //****************************************pa2*******************************************
-  size_t i = 0;
-  for (; dst[i] != 0; i++);
-  size_t len_dst = i;
-  for (; src[i - len_dst] != 0; i++) {
-    dst[i] = src[i - len_dst];
-  }
-  dst[i] = 0;
+  size_t pos_dst = strlen(dst);
+  size_t pos_src = 0;
+  for(; src[pos_src] != '\0'; pos_src++, pos_dst++)
+    dst[pos_dst] = src[pos_src];
+  dst[pos_dst] = '\0';
+
   return dst;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
+}
+
+char *strncat(char *dst, const char *src, size_t n) {
+  size_t pos_dst = strlen(dst);
+  size_t pos_src = 0;
+  for(; (pos_src) < n && (src[pos_src] != '\0'); pos_src++, pos_dst++)
+    dst[pos_dst] = src[pos_src];
+  dst[pos_dst] = '\0';
+
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  //****************************************pa2*******************************************
   size_t i = 0;
-  for (; s1[i] != 0 && s2[i] != 0; i++) {
-    if (s1[i] != s2[i]) {
-      return s1[i] - s2[i];
-    }
+  for(; s1[i] != '\0' && s2[i] != '\0'; ++i){
+    if(s1[i] > s2[i]) return 1;
+    else if(s1[i] == s2[i]) continue;
+    else return -1;
   }
-  return s1[i] - s2[i];
-  //****************************************pa2*******************************************
-  panic("Not implemented");
+
+  if(s1[i] == '\0' && s2[i] == '\0') return 0;
+  else if(s1[i] == '\0') return -1;
+  else return 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  //****************************************pa2*******************************************
   size_t i = 0;
-  for (; s1[i] != 0 && s2[i] != 0 && i < n; i++) {
-    if (s1[i] != s2[i]) {
-      return s1[i] - s2[i];
-    }
+  for(; i < n || (s1[i] != '\0' && s2[i] != '\0'); ++i){
+    if(s1[i] > s2[i]) return 1;
+    else if(s1[i] == s2[i]) continue;
+    else return -1;
   }
 
-  if (i == n) {
-    return 0;
-  }
-  return s1[i] - s2[i];
-  //****************************************pa2*******************************************
-  panic("Not implemented");
+  if((s1[i] != '\0' && s2[i] != '\0') || (s1[i] == '\0' && s2[i] == '\0')) return 0;
+  else if(s1[i] == '\0') return -1;
+  else return 1;
 }
 
 void *memset(void *s, int c, size_t n) {
-   //****************************************pa2*******************************************
-  for (size_t i = 0; i < n; i++) {
-    ((char*)s)[i] = (char)c;
+  for(size_t i = 0; i < n; ++i){
+    *((char*)s + i) = (char)c;
   }
   return s;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  //****************************************pa2*******************************************
-  char res[n];
-  for (size_t i = 0; i < n; i++) {
-    res[i] = *((char*)src + i);
-  }
-  for (size_t i = 0; i < n; i++) {
-    *((char*)dst + i) = res[i];
-  }
+  char tmp[n];
+  for(size_t i = 0; i < n; ++ i)
+    tmp[i] = *((char*)src + i);
+    
+  for(size_t i = 0; i < n; ++ i)
+    *((char*)dst + i ) = tmp[i];
+  
   return dst;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  //****************************************pa2*******************************************
-  for (size_t i = 0; i < n; i++) {
+  for(size_t i = 0; i < n; ++i)
     *((char*)out + i) = *((char*)in + i);
-  }
   return out;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  //****************************************pa2*******************************************
   for(int i = 0; i < n; ++i){
-    char val1 = *((char*)s1 + i);
-    char val2 = *((char*)s2 + i);
-    if(val1 != val2) {
-      return val1 - val2;
-    }
+    uint8_t val1 = *((uint8_t*)s1 + i);
+    uint8_t val2 = *((uint8_t*)s2 + i);
+    if(val1 > val2) return 1;
+    else if(val1 == val2) continue;
+    else return -1;
   }
   return 0;
-  //****************************************pa2*******************************************
-  panic("Not implemented");
 }
 
 #endif
