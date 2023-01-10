ed1c3eac7c0df1928edd32d7e84c8abe8112c204
tracer-ics2021 <tracer@njuics.org>
2023-01-10 17:20:25 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 8bb2a529b2404169ab84b30ee2bc2997   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:20:25 up 47 min,  1 user,  load average: 0.05, 0.04, 0.00

--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -1,3 +1,43 @@
+// def_EHelper(lw) {
+//   rtl_lm(s, ddest, dsrc1, id_src2->imm, 4);
+// }
+
+// def_EHelper(sw) {
+//   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
+// }
+
+// //***********************************pa2*************************************
+// def_EHelper(sh) {
+//   // R:把rs1-rs2半字写入rd
+//   rtl_sm(s, ddest, dsrc1, id_src2->imm, 2);
+// }
+
+// def_EHelper(lbu) {
+//   // R:从rs1+立即数符号扩展读取一个字节，经过0扩展后写入ddest
+//   rtl_lm(s, ddest, dsrc1, id_src2->imm, 1);
+// }
+
+// def_EHelper(lh) {
+//   // R:从rs1+立即数符号扩展读取半个字，经过符号扩展后写入ddest
+//   rtl_lms(s, ddest, dsrc1, id_src2->imm, 2);
+// }
+
+// def_EHelper(sb) {
+//   // R:从rs1+立即数符号扩展读取一个字节，经过0扩展后写入ddest
+//   rtl_sm(s, ddest, dsrc1, id_src2->imm, 1);
+// }
+
+// def_EHelper(lhu) {
+//   // R:从rs1+立即数符号扩展读取半个字，经过0扩展后写入ddest
+//   rtl_lm(s, ddest, dsrc1, id_src2->imm, 2);
+// }
+
+// def_EHelper(lb) {
+//   // R:从rs1+立即数符号扩展读取一个字节，经过符号扩展后写入ddest
+//   rtl_lms(s, ddest, dsrc1, id_src2->imm, 1);
+// }
+// //***********************************pa2*************************************
+
 def_EHelper(lw) {
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 4);
 }
@@ -6,34 +46,26 @@ def_EHelper(sw) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
 
-//***********************************pa2*************************************
 def_EHelper(sh) {
-  // R:把rs1-rs2半字写入rd
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 2);
 }
 
 def_EHelper(lbu) {
-  // R:从rs1+立即数符号扩展读取一个字节，经过0扩展后写入ddest
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 1);
 }
 
-def_EHelper(lh) {
-  // R:从rs1+立即数符号扩展读取半个字，经过符号扩展后写入ddest
-  rtl_lms(s, ddest, dsrc1, id_src2->imm, 2);
-}
-
 def_EHelper(sb) {
-  // R:从rs1+立即数符号扩展读取一个字节，经过0扩展后写入ddest
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 1);
 }
 
+def_EHelper(lh) {
+  rtl_lms(s, ddest, dsrc1, id_src2->imm, 2);
+}
+
 def_EHelper(lhu) {
-  // R:从rs1+立即数符号扩展读取半个字，经过0扩展后写入ddest
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 2);
 }
 
 def_EHelper(lb) {
-  // R:从rs1+立即数符号扩展读取一个字节，经过符号扩展后写入ddest
   rtl_lms(s, ddest, dsrc1, id_src2->imm, 1);
 }
-//***********************************pa2*************************************
