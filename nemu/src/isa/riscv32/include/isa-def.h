cc87d1f4f265b25d3daccc37561c29b0786ffd4c
tracer-ics2021 <tracer@njuics.org>
2023-01-10 17:13:56 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 8bb2a529b2404169ab84b30ee2bc2997   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:13:56 up 41 min,  1 user,  load average: 0.01, 0.01, 0.00

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -12,8 +12,78 @@ typedef struct {
 } riscv32_CPU_state;
 
 // decode
+// typedef struct {
+//   union {
+//     struct {
+//       uint32_t opcode1_0 : 2;
+//       uint32_t opcode6_2 : 5;
+//       uint32_t rd        : 5;
+//       uint32_t funct3    : 3;
+//       uint32_t rs1       : 5;
+//       int32_t  simm11_0  :12;
+//     } i;
+//     struct {
+//       uint32_t opcode1_0 : 2;
+//       uint32_t opcode6_2 : 5;
+//       uint32_t imm4_0    : 5;
+//       uint32_t funct3    : 3;
+//       uint32_t rs1       : 5;
+//       uint32_t rs2       : 5;
+//       int32_t  simm11_5  : 7;
+//     } s;
+//     struct {
+//       uint32_t opcode1_0 : 2;
+//       uint32_t opcode6_2 : 5;
+//       uint32_t rd        : 5;
+//       uint32_t imm31_12  :20;
+//     } u;
+//     //************************************pa2**************************************
+//     struct {
+//       uint32_t opcode1_0 : 2;
+//       uint32_t opcode6_2 : 5;
+//       uint32_t rd        : 5;
+//       uint32_t imm19_12  : 8;
+//       uint32_t imm11     : 1;
+//       uint32_t imm10_1   :10;
+//       uint32_t imm20     : 1;
+//     } j;
+//     struct {
+//       uint32_t opcode1_0 : 2;
+//       uint32_t opcode6_2 : 5;
+//       uint32_t rd        : 5;
+//       uint32_t funct3    : 3;
+//       uint32_t rs1       : 5;
+//       uint32_t rs2       : 5;
+//       int32_t  funct7    : 7;
+//     } r;
+//     struct {
+//       uint32_t opcode1_0 : 2;
+//       uint32_t opcode6_2 : 5;
+//       uint32_t imm11     : 1;
+//       uint32_t imm4_1    : 4;
+//       uint32_t funct3    : 3;
+//       uint32_t rs1       : 5;
+//       uint32_t rs2       : 5;
+//       uint32_t  imm10_5  : 6;
+//       uint32_t  imm12    : 1;
+//     } b;
+//     //************************************pa2**************************************
+//     uint32_t val;
+//   } instr;
+// } riscv32_ISADecodeInfo;
+
+// decode
 typedef struct {
   union {
+    struct {
+      uint32_t opcode1_0 : 2;
+      uint32_t opcode6_2 : 5;
+      uint32_t rd        : 5;
+      uint32_t funct3    : 3;
+      uint32_t rs1       : 5;
+      uint32_t rs2       : 5;
+      int32_t  funct7    : 7;
+    } r;
     struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
@@ -34,40 +104,29 @@ typedef struct {
     struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
+      uint32_t imm11     : 1;
+      uint32_t imm4_1    : 4;
+      uint32_t funct3    : 3;
+      uint32_t rs1       : 5;
+      uint32_t rs2       : 5;
+      uint32_t imm10_5   : 6;
+      int32_t  imm12    : 1;
+    } b;
+    struct {
+      uint32_t opcode1_0 : 2;
+      uint32_t opcode6_2 : 5;
       uint32_t rd        : 5;
       uint32_t imm31_12  :20;
     } u;
-    //************************************pa2**************************************
     struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
       uint32_t rd        : 5;
       uint32_t imm19_12  : 8;
       uint32_t imm11     : 1;
-      uint32_t imm10_1   :10;
-      uint32_t imm20     : 1;
+      uint32_t imm10_1   : 10;
+      int32_t  imm20    : 1;
     } j;
-    struct {
-      uint32_t opcode1_0 : 2;
-      uint32_t opcode6_2 : 5;
-      uint32_t rd        : 5;
-      uint32_t funct3    : 3;
-      uint32_t rs1       : 5;
-      uint32_t rs2       : 5;
-      int32_t  funct7    : 7;
-    } r;
-    struct {
-      uint32_t opcode1_0 : 2;
-      uint32_t opcode6_2 : 5;
-      uint32_t imm11     : 1;
-      uint32_t imm4_1    : 4;
-      uint32_t funct3    : 3;
-      uint32_t rs1       : 5;
-      uint32_t rs2       : 5;
-      uint32_t  imm10_5  : 6;
-      uint32_t  imm12    : 1;
-    } b;
-    //************************************pa2**************************************
     uint32_t val;
   } instr;
 } riscv32_ISADecodeInfo;
