73a5ea4d02c97eb4062d6bf10c9c731565d41cf7
tracer-ics2021 <tracer@njuics.org>
2023-01-10 14:09:10 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 8e4cb37096874a1dbce87405f1b80d83   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  14:09:10 up 6 min,  1 user,  load average: 0.05, 0.20, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,59 +1,18 @@
-// #include <am.h>
-// #include <nemu.h>
-
-// #define SYNC_ADDR (VGACTL_ADDR + 4)
-// //*****************************pa2********************************
-// #define SYNC_HEIGHT (VGACTL_ADDR)
-// //*****************************pa2********************************
-// void __am_gpu_init() {
-//   int i;
-//   int w = 0;  
-//   int h = 0;  
-//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-//   for (i = 0; i < w * h; i++) fb[i] = i;
-//   outl(SYNC_ADDR, 1);
-// }
-
-// void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-//   *cfg = (AM_GPU_CONFIG_T) {
-//     .present = true, .has_accel = false,
-//     .width = 0, .height = 0,
-//     .vmemsz = 0
-//   };
-// }
-
-// void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-//   if (ctl->sync) {
-//     outl(SYNC_ADDR, 1);
-//   }
-// }
-
-// void __am_gpu_status(AM_GPU_STATUS_T *status) {
-//   status->ready = true;
-// }
-
-
 #include <am.h>
-#include <klib.h>
-#include <klib-macros.h>
 #include <nemu.h>
-
-
-#include "../../../riscv/riscv.h"
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
-  // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
-  // printf("%d, %d", w, h);
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  //***********************************pa2***********************************
+  // *cfg = (AM_GPU_CONFIG_T) {
+  //   .present = true, .has_accel = false,
+  //   .width = 0, .height = 0,
+  //   .vmemsz = 0
+  // };
   uint32_t info = inl(VGACTL_ADDR);
   uint16_t height = (uint16_t)(info & 0xFFFF);
   uint16_t width = (uint16_t)(info >> 16);
@@ -62,22 +21,22 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .width = width, .height = height,
     .vmemsz = 0
   };
+  //***********************************pa2***********************************
+
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int win_weight = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  //***********************************pa2***********************************
+  int win_weight = io_read(AM_GPU_CONFIG).width;  
 
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pi = (uint32_t *)(uintptr_t)ctl->pixels;
-  // printf("(x:%d, y:%d) w=%d, h=%d", ctl->x, ctl->y, ctl->w, ctl->h);
   for (int i = 0; i < ctl->h; ++i){
     for (int j = 0; j < ctl->w; ++j){
       fb[(ctl->y) * win_weight + i * win_weight + ctl->x + j] = pi[i * (ctl->w) + j];
-      //真t娘的奇怪
-      //outl((uint32_t)(fb + ctl->y * win_weight + i * win_weight + ctl->x + j), pi[i * (ctl->w) + j]);
     }
   }
-  
+  //***********************************pa2***********************************
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
@@ -86,3 +45,55 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
 }
+
+
+// #include <am.h>
+// #include <klib.h>
+// #include <klib-macros.h>
+// #include <nemu.h>
+
+
+// #include "../../../riscv/riscv.h"
+
+// #define SYNC_ADDR (VGACTL_ADDR + 4)
+
+// void __am_gpu_init() {
+//   // int i;
+//   // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+//   // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+//   // printf("%d, %d", w, h);
+//   // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//   // for (i = 0; i < w * h; i ++) fb[i] = i;
+//   // outl(SYNC_ADDR, 1);
+// }
+
+// void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+//   uint32_t info = inl(VGACTL_ADDR);
+//   uint16_t height = (uint16_t)(info & 0xFFFF);
+//   uint16_t width = (uint16_t)(info >> 16);
+//   *cfg = (AM_GPU_CONFIG_T) {
+//     .present = true, .has_accel = false,
+//     .width = width, .height = height,
+//     .vmemsz = 0
+//   };
+// }
+
+// void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+//   int win_weight = io_read(AM_GPU_CONFIG).width;  
+
+//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//   uint32_t *pi = (uint32_t *)(uintptr_t)ctl->pixels;
+//   for (int i = 0; i < ctl->h; ++i){
+//     for (int j = 0; j < ctl->w; ++j){
+//       fb[(ctl->y) * win_weight + i * win_weight + ctl->x + j] = pi[i * (ctl->w) + j];
+//     }
+//   }
+  
+//   if (ctl->sync) {
+//     outl(SYNC_ADDR, 1);
+//   }
+// }
+
+// void __am_gpu_status(AM_GPU_STATUS_T *status) {
+//   status->ready = true;
+// }
