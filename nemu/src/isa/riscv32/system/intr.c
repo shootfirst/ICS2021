c3c92253db248a8cbde92d21b79f165a6935ec15
tracer-ics2021 <tracer@njuics.org>
2023-01-10 20:12:20 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 8bb2a529b2404169ab84b30ee2bc2997   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  20:12:20 up  2:17,  1 user,  load average: 0.13, 0.06, 0.02

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,6 +4,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  //***********************************pa3***********************************
+
+  //***********************************pa3***********************************
 
   return 0;
 }
