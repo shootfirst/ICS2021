cddc3b5abe377b5b3294f0809882b03bbb148417
tracer-ics2021 <tracer@njuics.org>
2023-01-12 17:19:49 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 7b1dda7febec4257bb75f1e5c89b0eff   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:19:49 up  2:04,  1 user,  load average: 0.17, 0.10, 0.04

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -52,7 +52,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-  assert(1 == SYS_exit);
+  while(1);
   _syscall_(SYS_exit, status, 0, 0);
   while (1);
 }
