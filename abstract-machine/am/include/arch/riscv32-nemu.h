8306d453539486c81411c66b3207dbb16f4e1496
tracer-ics2021 <tracer@njuics.org>
2023-01-12 20:16:18 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 7b1dda7febec4257bb75f1e5c89b0eff   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  20:16:18 up  3:21,  1 user,  load average: 0.43, 0.24, 0.09

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -4,7 +4,7 @@
 struct Context {
   // TODO: fix the order of these members to match trap.S
   //******************************************pa3***********************************************
-  uintptr_t gpr[32], mepc, mcause, mstatus;
+  uintptr_t gpr[32], mcause, mstatus, mepc;
   //******************************************pa3***********************************************
   void *pdir;
 };
@@ -16,22 +16,4 @@ struct Context {
 #define GPRx gpr[10]
 
 #endif
-
-// #ifndef ARCH_H__
-// #define ARCH_H__
-
-// struct Context
-// {
-//   // TODO: fix the order of these members to match trap.S
-//   uintptr_t gpr[32], mcause, mstatus, mepc;
-//   void *pdir;
-// };
-
-// #define GPR1 gpr[17] // a7
-// #define GPR2 gpr[10]
-// #define GPR3 gpr[11]
-// #define GPR4 gpr[12]
-// #define GPRx gpr[10]
-
-// #endif
 
