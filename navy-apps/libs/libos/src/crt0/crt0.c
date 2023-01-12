6fd4a741bdea7940ab3dcc84ddf352266d962ef3
tracer-ics2021 <tracer@njuics.org>
2023-01-12 17:21:13 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 7b1dda7febec4257bb75f1e5c89b0eff   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:21:13 up  2:05,  1 user,  load average: 0.20, 0.12, 0.05

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,6 +7,7 @@ extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
+  printf("11111111111111111111\n");
   exit(main(0, empty, empty));
   assert(0);
 }
