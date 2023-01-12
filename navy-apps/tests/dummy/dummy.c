5563cfdd95801d936bd6abad70028e04d59b8a2c
tracer-ics2021 <tracer@njuics.org>
2023-01-12 16:00:15 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 7b1dda7febec4257bb75f1e5c89b0eff   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:00:15 up 44 min,  1 user,  load average: 0.05, 0.12, 0.14

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
@@ -8,5 +9,9 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  return _syscall_(SYS_yield, 0, 0, 0);
+  printf("ggg1\n");
+  int ans = _syscall_(SYS_yield, 0, 0, 0);
+  printf("ggg2\n");
+  return ans;
+  // return _syscall_(SYS_yield, 0, 0, 0);
 }
