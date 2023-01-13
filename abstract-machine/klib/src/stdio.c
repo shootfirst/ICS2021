9723b3c6da34cc5cee8957618270d9d84cb3d3aa
tracer-ics2021 <tracer@njuics.org>
2023-01-13 17:42:24 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 372fe6b5ab6d4bffb7f54640a8570aac   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:42:24 up  5:40,  1 user,  load average: 0.02, 0.07, 0.04

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -352,14 +352,116 @@ int sprintf(char *out, const char *fmt, ...)
   return num;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
-  panic("Not implemented");
+// int snprintf(char *out, size_t n, const char *fmt, ...)
+// {
+//   panic("Not implemented");
+// }
+
+// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+// {
+//   panic("Not implemented");
+// }
+
+static char HEX_CHARACTERS[] = "0123456789ABCDEF";
+#define BIT_WIDE_HEX 8
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list arg;
+  va_start (arg, fmt);
+
+  int done = vsnprintf(out, n, fmt, arg);
+
+  va_end(arg);
+  return done;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
-  panic("Not implemented");
+#define append(x) {out[j++]=x; if (j >= n) {break;}}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  char buffer[128];
+  char *txt, cha;
+  int num, len;
+  unsigned int unum;
+  uint32_t pointer;
+  
+  
+  int state = 0, i, j;//模仿一个状态机
+  for (i = 0, j = 0; fmt[i] != '\0'; ++i){
+    switch (state)
+    {
+    case 0:
+      if (fmt[i] != '%'){
+        append(fmt[i]);
+      } else
+        state = 1;
+      break;
+    
+    case 1:
+      switch (fmt[i])
+      {
+      case 's':
+        txt = va_arg(ap, char*);
+        for (int k = 0; txt[k] !='\0'; ++k)
+          append(txt[k]);
+        break;
+      
+      case 'd':
+        num = va_arg(ap, int);
+        if(num == 0){
+          append('0');
+          break;
+        }
+        if (num < 0){
+          append('-');
+          num = 0 - num;
+        }
+        for (len = 0; num ; num /= 10, ++len)
+          //buffer[len] = num % 10 + '0';//逆序的
+          buffer[len] = HEX_CHARACTERS[num % 10];//逆序的
+        for (int k = len - 1; k >= 0; --k)
+          append(buffer[k]);
+        break;
+      
+      case 'c':
+        cha = (char)va_arg(ap, int);
+        append(cha);
+        break;
+
+      case 'p':
+        pointer = va_arg(ap, uint32_t);
+        for (len = 0; pointer ; pointer /= 16, ++len)
+          buffer[len] = HEX_CHARACTERS[pointer % 16];//逆序的
+        for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
+          append('0');
+
+        for (int k = len - 1; k >= 0; --k)
+          append(buffer[k]);
+        break;
+
+      case 'x':
+        unum = va_arg(ap, unsigned int);
+        if(unum == 0){
+          append('0');
+          break;
+        }
+        for (len = 0; unum ; unum >>= 4, ++len)
+          buffer[len] = HEX_CHARACTERS[unum & 0xF];//逆序的
+
+        for (int k = len - 1; k >= 0; --k)
+          append(buffer[k]);
+        break;  
+
+      default:
+        assert(0);
+      }
+      state = 0;
+      break;
+      
+    }
+  }
+
+  out[j] = '\0';
+  return j;
 }
 
 #endif
