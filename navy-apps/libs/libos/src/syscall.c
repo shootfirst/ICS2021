1a7488d8f3ae3d1979ea79403ca587e65a7ab221
tracer-ics2021 <tracer@njuics.org>
2023-01-13 09:19:33 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 372fe6b5ab6d4bffb7f54640a8570aac   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  09:19:33 up 19 min,  1 user,  load average: 0.12, 0.08, 0.08

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -69,11 +69,29 @@ int _write(int fd, void *buf, size_t count) {
   return 0;
 }
 
+// void *_sbrk(intptr_t increment) {
+//   //**************************************pa3**************************************
+//   return _syscall_(SYS_brk, increment, 0, 0);
+//   //**************************************pa3**************************************
+//   return (void *)-1;
+// }
+
+extern char end;
+void * program_break = NULL;
 void *_sbrk(intptr_t increment) {
-  //**************************************pa3**************************************
-  return _syscall_(SYS_brk, increment, 0, 0);
-  //**************************************pa3**************************************
-  return (void *)-1;
+  if (program_break == NULL){// 初始化
+    program_break = &end;
+  }
+  void *old_program_break = program_break;
+  
+  int ret = _syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
+  if (ret == 0){
+    program_break = program_break + increment;
+  }else {
+    assert(0);
+  }
+  
+  return old_program_break;
 }
 
 int _read(int fd, void *buf, size_t count) {
