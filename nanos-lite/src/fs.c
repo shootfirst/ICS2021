61d5d63becca353027f953a6cb372908bbcc0ae8
tracer-ics2021 <tracer@njuics.org>
2023-01-13 20:34:35 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 372fe6b5ab6d4bffb7f54640a8570aac   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  20:34:35 up  6:19,  1 user,  load average: 0.47, 0.34, 0.18

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,11 +50,11 @@ static Finfo file_table[] __attribute__((used)) = {
   
   // 文件
   {"/bin/hello", 33424, 400143, NULL, NULL},
-  {"/bin/event-test", 58840, 433567, NULL, NULL},
-  {"/bin/time-test", 58808, 492407, NULL, NULL},
-  {"/bin/file-test", 48328, 551215, NULL, NULL},
-  {"/bin/dummy", 29068, 599543, NULL, NULL},
-  {"/bin/bmp-test", 62956, 628611, NULL, NULL},
+{"/bin/event-test", 43240, 433567, NULL, NULL},
+{"/bin/time-test", 43240, 476807, NULL, NULL},
+{"/bin/file-test", 48328, 520047, NULL, NULL},
+{"/bin/dummy", 29068, 568375, NULL, NULL},
+{"/bin/bmp-test", 48116, 597443, NULL, NULL},
   //******************************************pa3******************************************
 #include "files.h"
 };
