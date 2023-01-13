1c30d3774fdff891b1a87afb9373c0c1edb39e3c
tracer-ics2021 <tracer@njuics.org>
2023-01-13 16:44:32 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 372fe6b5ab6d4bffb7f54640a8570aac   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:44:32 up  4:43,  1 user,  load average: 0.29, 0.20, 0.11

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,23 +24,47 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   //**********************************pa3**************************************
 }
 
-size_t events_read(void *buf, size_t offset, size_t len) {
-  //*********************************pa3***************************************
-  // AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
-  // if (AM_KEY_NONE == kbd.keycode) {
-  //   return 0;
-  // }
-  AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
-  if (kbd.keycode == AM_KEY_NONE) return 0;
-  printf("hehe\n");
-  char res[64];
-  memset(res, 0, 64);
-  assert(sprintf(res, "Got  (kbd): %s (%d) %s\n", keyname[kbd.keycode], kbd.keycode, kbd.keydown ? "DOWN" : "UP") <= 64);
-  size_t real_len = strlen(res);
-  strcpy(buf, res);
-  return real_len;
+// size_t events_read(void *buf, size_t offset, size_t len) {
+//   //*********************************pa3***************************************
+//   AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
+//   if (AM_KEY_NONE == kbd.keycode) {
+//     return 0;
+//   }
+//   printf("hehe\n");
+//   char res[64];
+//   memset(res, 0, 64);
+//   assert(sprintf(res, "Got  (kbd): %s (%d) %s\n", keyname[kbd.keycode], kbd.keycode, kbd.keydown ? "DOWN" : "UP") <= 64);
+//   size_t real_len = strlen(res);
+//   strcpy(buf, res);
+//   return real_len;
 
-  //*********************************pa3***************************************
+//   //*********************************pa3***************************************
+// }
+
+size_t events_read(void *buf, size_t offset, size_t len) {
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE) return 0;
+  
+  int real_length = 4;
+  char *tag = ev.keydown ? "kd " : "ku ";
+  if (real_length <= len){
+    strcpy(buf, tag);
+  }else {
+    assert(0);
+    return 0;
+  }
+  real_length += strlen(keyname[ev.keycode]);
+  
+  if (real_length<= len){
+    strcat(buf, keyname[ev.keycode]);
+  }else {
+    Log("Need %d for %s%s but got %d", strlen(keyname[ev.keycode]), buf, keyname[ev.keycode], len);
+    assert(0);
+    return 0;
+  }
+  Log("Got  (kbd): %s (%d) %s\n", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
+  
+  return real_length;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
