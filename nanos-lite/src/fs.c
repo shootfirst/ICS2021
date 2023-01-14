633c16b835eaf9350e35ccb9d3d359e8f9b3f6ce
tracer-ics2021 <tracer@njuics.org>
2023-01-14 21:25:59 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 410afdab5bed42ff9ae19397345affa9   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:25:59 up 28 min,  1 user,  load average: 0.13, 0.17, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,11 +50,11 @@ static Finfo file_table[] __attribute__((used)) = {
   
   // 文件
   {"/bin/hello", 33424, 400143},
-{"/bin/event-test", 58980, 433567},
-{"/bin/time-test", 58948, 492547},
-{"/bin/file-test", 48328, 551495},
-{"/bin/dummy", 29068, 599823},
-{"/bin/bmp-test", 63096, 628891},
+{"/bin/event-test", 58908, 433567},
+{"/bin/time-test", 58884, 492475},
+{"/bin/file-test", 48328, 551359},
+{"/bin/dummy", 29068, 599687},
+{"/bin/bmp-test", 63032, 628755},
   //******************************************pa3******************************************
 #include "files.h"
 };
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+//********************************pa3**********************************
 #include <sys/time.h>
-#include <assert.h>
 #include <fcntl.h>
+#include <assert.h>
 
 #define IS_NUM(ch) (ch >= '0' && ch <= '9')
-
-static int evtdev = -1;
-static int fbdev = -1;
 static int dispinfo_dev = -1;
-static int screen_w = 0, screen_h = 0;
-
 typedef struct size
 {
   int w;
   int h;
 } Size;
 Size disp_size;
+//********************************pa3**********************************
+static int evtdev = -1;
+static int fbdev = -1;
+static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
   //********************************pa3**********************************
@@ -32,7 +32,9 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   //********************************pa3**********************************
-  size_t read_len = read(evtdev, buf, len);
+  int fd = open("/dev/events", 0, 0);
+  size_t read_len = read(fd, buf, len);
+  close(fd);
   return read_len;
   //********************************pa3**********************************
 }
@@ -79,37 +81,161 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
   }
   assert(w > 0 && w <= disp_size.w);
   assert(h > 0 && h <= disp_size.h);
-
-  // write(1, "here\n", 10);
-  // printf("draw [%d, %d] to [%d, %d]\n", w, h, x, y);
   for (size_t row = 0; row < h; ++row)
   {
-    // printf("draw row %d with len %d\n", row, w);
     lseek(fbdev, x + (y + row) * disp_size.w, SEEK_SET);
-    // printf("pixels pos %p with len %d\n",pixels + row * w, w);
     write(fbdev, pixels + row * w, w);
-    // printf("draw row %d with len %d\n", row, w);
   }
   write(fbdev, 0, 0);
 }
 
-void NDL_OpenAudio(int freq, int channels, int samples)
-{
-}
 
-void NDL_CloseAudio()
-{
-}
 
-int NDL_PlayAudio(void *buf, int len)
-{
+
+int NDL_Init(uint32_t flags) {
+  if (getenv("NWM_APP")) {
+    evtdev = 3;
+  }
+  //*****************************pa3*********************************
+  char width_height[64];
+  int fd = open("/proc/dispinfo", 0, 0);
+  assert(read(fd, width_height, sizeof(width_height)));
+  // 格式： 宽度,高度
+  char *width = strtok(width_height, ",");
+  char *height = width_height + strlen(width_height) + 1;
+  sscanf(width, "%d", &screen_w);
+  sscanf(height, "%d", &screen_h);
+  printf("width:%d, height:%d\n", screen_w, screen_h);
+  //*****************************pa3*********************************
   return 0;
 }
 
-int NDL_QueryAudio()
-{
+void NDL_Quit() {
+}
+
+void NDL_OpenAudio(int freq, int channels, int samples) {
+}
+
+void NDL_CloseAudio() {
+}
+
+int NDL_PlayAudio(void *buf, int len) {
   return 0;
 }
+
+int NDL_QueryAudio() {
+  return 0;
+}
+
+
+
+
+// #include <stdint.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <sys/time.h>
+// #include <assert.h>
+// #include <fcntl.h>
+
+// #define IS_NUM(ch) (ch >= '0' && ch <= '9')
+
+// static int evtdev = -1;
+// static int fbdev = -1;
+// static int dispinfo_dev = -1;
+// static int screen_w = 0, screen_h = 0;
+
+// typedef struct size
+// {
+//   int w;
+//   int h;
+// } Size;
+// Size disp_size;
+
+// uint32_t NDL_GetTicks() {
+//   //********************************pa3**********************************
+//   struct timeval tv;
+//   gettimeofday(&tv, NULL);
+//   // 返回毫秒数
+//   return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+//   //********************************pa3**********************************
+// }
+
+// int NDL_PollEvent(char *buf, int len) {
+//   //********************************pa3**********************************
+//   size_t read_len = read(evtdev, buf, len);
+//   return read_len;
+//   //********************************pa3**********************************
+// }
+
+// void NDL_OpenCanvas(int *w, int *h)
+// {
+//   if (*w == 0 && *h == 0)
+//   {
+//     *w = disp_size.w;
+//     *h = disp_size.h;
+//   }
+
+//   if (getenv("NWM_APP"))
+//   {
+//     int fbctl = 4;
+//     fbdev = 5;
+//     screen_w = *w;
+//     screen_h = *h;
+//     char buf[64];
+//     int len = sprintf(buf, "%d %d", screen_w, screen_h);
+//     // let NWM resize the window and create the frame buffer
+//     write(fbctl, buf, len);
+//     while (1)
+//     {
+//       // 3 = evtdev
+//       int nread = read(3, buf, sizeof(buf) - 1);
+//       if (nread <= 0)
+//         continue;
+//       buf[nread] = '\0';
+//       if (strcmp(buf, "mmap ok") == 0)
+//         break;
+//     }
+
+//     close(fbctl);
+//   }
+// }
+
+// void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
+// {
+//   if (w == 0 && h == 0)
+//   {
+//     w = disp_size.w;
+//     h = disp_size.h;
+//   }
+//   assert(w > 0 && w <= disp_size.w);
+//   assert(h > 0 && h <= disp_size.h);
+//   for (size_t row = 0; row < h; ++row)
+//   {
+//     lseek(fbdev, x + (y + row) * disp_size.w, SEEK_SET);
+//     write(fbdev, pixels + row * w, w);
+//   }
+//   write(fbdev, 0, 0);
+// }
+
+// void NDL_OpenAudio(int freq, int channels, int samples)
+// {
+// }
+
+// void NDL_CloseAudio()
+// {
+// }
+
+// int NDL_PlayAudio(void *buf, int len)
+// {
+//   return 0;
+// }
+
+// int NDL_QueryAudio()
+// {
+//   return 0;
+// }
 
 // int NDL_Init(uint32_t flags)
 // {
@@ -117,46 +243,26 @@ int NDL_QueryAudio()
 //   {
 //     evtdev = 3;
 //   }
+//   //*****************************pa3*********************************
+//   // open 3 dev file
 //   evtdev = open("/dev/events", 0, 0);
 //   fbdev = open("/dev/fb", 0, 0);
 //   dispinfo_dev = open("/proc/dispinfo", 0, 0);
 
-//   FILE *fp = fopen("/proc/dispinfo", "r");
-//   fscanf(fp, "WIDTH:%d\nHEIGHT:%d\n", &disp_size.w, &disp_size.h);
-  
+//   char width_height[64];
+//   assert(read(dispinfo_dev, width_height, sizeof(width_height)));
+//   // 格式： 宽度,高度
+//   char *width = strtok(width_height, ",");
+//   char *height = width_height + strlen(width_height) + 1;
+//   sscanf(width, "%d", &disp_size.w);
+//   sscanf(height, "%d", &disp_size.h);
 //   printf("width:%d, height:%d\n", disp_size.w, disp_size.h);
-//   assert(disp_size.w >= 400 && disp_size.w <= 800);
-//   assert(disp_size.h >= 300 && disp_size.h <= 640);
-//   fclose(fp);
 //   return 0;
+//   //*****************************pa3*********************************
 // }
 
-int NDL_Init(uint32_t flags)
-{
-  if (getenv("NWM_APP"))
-  {
-    evtdev = 3;
-  }
-  //*****************************pa3*********************************
-  // open 3 dev file
-  evtdev = open("/dev/events", 0, 0);
-  fbdev = open("/dev/fb", 0, 0);
-  dispinfo_dev = open("/proc/dispinfo", 0, 0);
-
-  char width_height[64];
-  assert(read(dispinfo_dev, width_height, sizeof(width_height)));
-  // 格式： 宽度,高度
-  char *width = strtok(width_height, ",");
-  char *height = width_height + strlen(width_height) + 1;
-  sscanf(width, "%d", &disp_size.w);
-  sscanf(height, "%d", &disp_size.h);
-  printf("width:%d, height:%d\n", disp_size.w, disp_size.h);
-  return 0;
-  //*****************************pa3*********************************
-}
-
-void NDL_Quit()
-{
-  close(evtdev);
-  close(dispinfo_dev);
-}
+// void NDL_Quit()
+// {
+//   close(evtdev);
+//   close(dispinfo_dev);
+// }
