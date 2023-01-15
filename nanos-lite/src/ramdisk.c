bc277d4ee7b40c664b6f4abe2e14d8860dbd7844
tracer-ics2021 <tracer@njuics.org>
2023-01-15 22:00:19 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 1d3b7c99da2c48059cdfa20ed15eee25   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  22:00:19 up  2:40,  1 user,  load average: 0.05, 0.06, 0.01

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -11,6 +11,7 @@ extern uint8_t ramdisk_end;
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
+  printf("%d, %d", offset, len);
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
