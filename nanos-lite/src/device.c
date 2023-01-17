3bf739491dbe8a128ce76896f1b9109903961150
tracer-ics2021 <tracer@njuics.org>
2023-01-17 16:00:06 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: dc0006fc7207414d9bdfd4b2d747e7bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:00:06 up 47 min,  1 user,  load average: 0.20, 0.35, 0.33

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -43,8 +43,10 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   //*********************************pa3***************************************
   AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
   if (AM_KEY_NONE == kbd.keycode) {
+    printf("11111111111111111111\n");
     return 0;
   }
+  printf("22222222222222222222222\n");
   char res[64];
   memset(res, 0, 64);
   if (kbd.keydown) {
