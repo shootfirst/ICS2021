6a289236ab99e2c7cb832b83a37ea8ef17c2b2a7
tracer-ics2021 <tracer@njuics.org>
2023-01-17 15:55:43 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: dc0006fc7207414d9bdfd4b2d747e7bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:55:43 up 42 min,  1 user,  load average: 0.57, 0.55, 0.37

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -5,8 +5,8 @@ int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
+    printf("hhhhhhhhhhhhhhhhhhhhhhhh\n");
     if (NDL_PollEvent(buf, sizeof(buf))) {
-      printf("hhhhhhhhhhhhhhhhhhhhhhhh\n");
       printf("receive event: %s\n", buf);
     }
   }
