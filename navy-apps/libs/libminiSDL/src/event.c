5de926b27db74f7929d7b99df5a308ca9882a392
tracer-ics2021 <tracer@njuics.org>
2023-01-18 10:45:27 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 72b4576faff344b69f450abdd58e1e66   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  10:45:27 up 32 min,  1 user,  load average: 0.94, 0.60, 0.45

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -68,8 +68,7 @@ int SDL_WaitEvent(SDL_Event *event) {
   }
 
   for(size_t i = 0; i < NR_KEYS; ++i){
-    printf("mypress is %s keyname %s\n", buf + 3, keyname[i]);
-    if(((strlen(buf + 3) - 1) == strlen(keyname[i])) && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0)) {
+    if(((strlen(buf + 3)) == strlen(keyname[i])) && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0)) {
       printf("keyname hit %s\n", keyname[i]);
       event->key.keysym.sym = i; 
       keysnap[i] = (event->type == SDL_KEYDOWN)? 1: 0;
