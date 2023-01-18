9f65e75b930d8cd14c1eb4e719fa2523345c12ac
tracer-ics2021 <tracer@njuics.org>
2023-01-18 12:05:37 +0800

    >  compile 201220000  Static hostname: shootsecond        Icon name: computer-vm          Chassis: vm       Machine ID: 603187c143334fffadba48779198ee6f          Boot ID: 39890ba12472466ea647f82ba10b7264   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  12:05:38 up 26 min,  1 user,  load average: 0.54, 0.51, 0.52

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -1,49 +1,3 @@
-// #include <nterm.h>
-// #include <stdarg.h>
-// #include <unistd.h>
-// #include <SDL.h>
-
-// char handle_key(SDL_Event *ev);
-
-// static void sh_printf(const char *format, ...) {
-//   static char buf[256] = {};
-//   va_list ap;
-//   va_start(ap, format);
-//   int len = vsnprintf(buf, 256, format, ap);
-//   va_end(ap);
-//   term->write(buf, len);
-// }
-
-// static void sh_banner() {
-//   sh_printf("Built-in Shell in NTerm (NJU Terminal)\n\n");
-// }
-
-// static void sh_prompt() {
-//   sh_printf("sh> ");
-// }
-
-// static void sh_handle_cmd(const char *cmd) {
-// }
-
-// void builtin_sh_run() {
-//   sh_banner();
-//   sh_prompt();
-
-//   while (1) {
-//     SDL_Event ev;
-//     if (SDL_PollEvent(&ev)) {
-//       if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
-//         const char *res = term->keypress(handle_key(&ev));
-//         if (res) {
-//           sh_handle_cmd(res);
-//           sh_prompt();
-//         }
-//       }
-//     }
-//     refresh_terminal();
-//   }
-// }
-
 #include <nterm.h>
 #include <stdarg.h>
 #include <unistd.h>
@@ -69,24 +23,15 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
-  char exe_path[64];
-  strcpy(exe_path, cmd);
-  exe_path[strlen(exe_path) - 1] = '\0';//把'\n'搞掉
-
-  printf("%s\n", exe_path);
-  execvp(exe_path, NULL);
 }
 
 void builtin_sh_run() {
   sh_banner();
   sh_prompt();
 
-  setenv("PATH", "/bin", 0);
   while (1) {
     SDL_Event ev;
-    int ret = SDL_PollEvent(&ev);
-    
-    if (ret) {
+    if (SDL_PollEvent(&ev)) {
       if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
         const char *res = term->keypress(handle_key(&ev));
         if (res) {
